ef458d26731364bd28218c015c03a12c04fe13f3
Joel Oliveira <58565967+joelpoliveira@users.noreply.github.com>
2022-11-19 15:39:03 +0000

    refactor (insert_data.py): mongodb insertion according to new schema

--- src/mongodb/insert_data.py
+++ src/mongodb/insert_data.py
@@ -1,14 +1,26 @@
 from pymongo import MongoClient
+from pymongo.collection import Collection
 from pymongo.errors import CollectionInvalid
 import json
 import os
 
 client = MongoClient()
 db = client.Project_BDA
-try:
-    movies = db.create_collection("Movies")
-except CollectionInvalid:
-    movies = db["Movies"]
+
+def get_collection(name: str) -> Collection:
+    try:
+        collection = db.create_collection(name)
+    except CollectionInvalid:
+        collection = db[name]
+    return collection   
+
+# with open("../../schemas/MongoSchema.json", "r") as f:
+#     schemas = json.load(f)["schemas"]
+
+coll_movies=get_collection("Movies")
+coll_genomes = get_collection("Genome Scores")
+coll_tags = get_collection("Tags")
+coll_ratings = get_collection("Ratings")
     
 files = os.listdir("data")
 files = list(
@@ -30,7 +42,18 @@ for file in sorted(files, key=lambda x: key[x]):
             try:
                 print(f"Inserting {file}")
                 data = json.load(f)
-                movies.insert_many(data)
+                for movie in data:
+                    temp = movie["genome_scores"]
+                    movie["genome_scores"] = coll_genomes.insert_many(temp).inserted_ids
+                    
+                    temp = movie["tags"]
+                    movie["tags"] = coll_tags.insert_many(temp).inserted_ids
+
+                    temp = movie["ratings"]
+                    movie["ratings"] = coll_ratings.insert_many(temp).inserted_ids
+
+                    movie["_id"] = movie.pop("movieId")
+                    coll_movies.insert_one(movie)
                 f.close()
             except Exception as e:
                 print("Error Ocurred")
