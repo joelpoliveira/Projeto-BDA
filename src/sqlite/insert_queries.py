81181d002307be86c8aabe4d1574b7c1b7f352e0
Joel Oliveira <58565967+joelpoliveira@users.noreply.github.com>
2022-11-23 15:19:51 +0000

    add: update query-> update rating based on userId

---
+++ src/sqlite/insert_queries.py
@@ -0,0 +1,23 @@
+import sqlite3
+from time import time
+
+db = sqlite3.connect("./database/Project_BDA.db")
+
+userid=134; rating = 3.5;
+query = """UPDATE Ratings
+           SET rating=%f
+           WHERE userid=%d
+""" % (rating, userid)
+
+try:
+    cur = db.cursor()
+    start = time()
+    cur.execute(query)
+    print(time() - start)
+
+    cur.close()
+    db.commit()
+    
+except Exception as e:
+    db.rollback()
+    print(e)
