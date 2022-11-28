825ba80381ceec822d32c1825e2f6b381583ba1f
Joel Oliveira <58565967+joelpoliveira@users.noreply.github.com>
2022-11-28 14:40:07 +0000

    add second complex query

--- src/mongodb/complex_queries.py
+++ src/mongodb/complex_queries.py
@@ -7,19 +7,37 @@ db = client.Project_BDA
 #Selecionar os filmes com maior 'rating'.
 #Os filmes em consideração para a pesquisa 
 #são que têm uma certa tag como a mais relevante
-tag = "007"
-query = [   { "$sort" : {"relevance": -1}},
+tag = "007 (series)"
+query = [   
             {"$group" : {
                     "_id" : "$movieId",
                     "relevance" : {"$max" : "$relevance"},
-                    "tag" : { "$first" : "$tag" }
                 }
             },
-            { "$match" : {"tag" : f"{tag}"}},
-            { "$project" : {
+            {"$lookup" : {
+                "from" : "Genome_Scores",
+                "let" : {"movieid":"$_id", "rel":"$relevance"},
+                "pipeline":[{
+                    "$match" : {"$expr" : {
+                        "$and" : [
+                            {"$eq" : ["$movieId", "$$movieid"]},
+                            {"$eq" : ["$relevance", "$$rel"]}
+                        ]
+                    }}
+                }],
+                "as" : "records"
+                }
+            },
+            {"$unwind" : "$records"},
+            { "$match" : {"records.tag" : tag}},
+            # { "$project" : {
+            #     "_id" : 1,
+            #     "tag" : "$records.tag"
+            # }},
+            # {"$match" : {"tag" : tag}},
+            {"$project" : {
                 "_id" : 1,
-                "relevance" : 0,
-                "tag" : 0
+                # "tag" : 0
             }},
             {"$lookup" : {
                 "from" : "Ratings",
@@ -45,18 +63,107 @@ query = [   { "$sort" : {"relevance": -1}},
             {"$unwind" : "$movie"},
             {"$project" : {
                 "_id" : 0,
-                "movie.title" : 1,
+                "title" : "$movie.title",
                 "rating" : 1
-            }}
+            }},
+            {"$sort" : {"rating" : -1}}
+            # {"$limit" : 5}
         ]
 
+#Obter os filmes cujas tags por 
+# utilizador mais frequentes
+# coincidem com as de um
+# certo filme escolhido à priori
+movieid = 1;
+subsubquery = [
+    {"$project" : {
+        "_id" : 0,
+        "movieId" : 1,
+        "tag" : {"$toLower" : {"$trim" : {"input": "$tag"}}},
+    }},
+    {"$group" : {
+        "_id" : {
+            "movieid" : "$movieId", 
+            "tag" : "$tag"
+        },
+        "counts" : {"$sum" : 1}
+    }},
+]
+subquery = [
+    *subsubquery,
+    {"$group" : {
+        "_id" : "$_id.movieid",
+        "counts" : {"$max" : "$counts"}
+    }},
+    {"$lookup" : {
+        "from" : "Tags",
+        "let" : {
+            "movieid": "$_id", 
+            "counts" : "$counts"
+        },
+        "pipeline" : [
+            *subsubquery,
+            {"$match" : {
+                "$expr" : {
+                    "$and" : [
+                        {"$eq" : ["$_id.movieid", "$$movieid"]},
+                        {"$eq" : ["$counts", "$$counts"]}
+                    ]
+                }
+            }},
+        ],
+        "as" : "tags"
+    }},
+    {"$unwind":"$tags"},
+    # {"$limit" : 10}
+]
+query2 = [
+    *subquery,
+    {"$match" : {"_id" : movieid}},
+    {"$project" : {
+        "_id" : "$_id",
+        "tag" : "$tags._id.tag",
+    }},
+    # {"$lookup" : {
+    #     "from" : "Tags",
+    #     "let" : {
+    #         "movieid" : "$_id",
+    #         "tag" : "$tag"
+    #     },
+    #     "pipeline" : [
+    #         *subquery,
+    #         {"$match" : {"_id" : {"$not" : {"$eq" : "$$movieid"}}}},
+    #         {"$project" : {
+    #             "_id" : "$_id",
+    #             "tag" : "$tags._id.tag",
+    #         }},
+    #     ],
+    #     "as" :"matches"
+    # }},
+    # {"$unwind" : "$matches"},
+    # {"$project" : {
+    #     "_id1" : "$_id",
+    #     "tag1" : "$tag",
+    #     "_id2" : "$matches._id",
+    #     "tag2" : "$matches._tag"
+    # }},
+    # {"$match" : {
+    #     "$expr" : {
+    #         "$ne" : ["$_id", 1]
+    #         }
+    #     }
+    # },
+    {"$limit" : 10}
+]
 
 coll_gen = db["Genome_Scores"]
 coll_ratings = db["Ratings"]
 coll_movies = db["Movies"]
+coll_tags = db["Tags"]
 
 start = time()
 docs = coll_gen.aggregate(query)
+# docs = coll_tags.aggregate(query2)
 print(time() - start)
 
 input()
--- src/sqlite/complex_queries.py
+++ src/sqlite/complex_queries.py
@@ -6,7 +6,7 @@ db = sqlite3.connect("./database/Project_BDA.db")
 #Selecionar os filmes com maior 'rating'.
 #Os filmes em consideração para a pesquisa 
 #são que têm uma certa tag como a mais relevante
-tagid = 1
+tagid = 2
 query = """ SELECT Movies.title, AVG(rating) AS avg_rating
              FROM Ratings
              INNER JOIN MOVIES
@@ -16,19 +16,64 @@ query = """ SELECT Movies.title, AVG(rating) AS avg_rating
                     AND Movies.movieid in (
                         SELECT movieid
                         FROM (
-                            SELECT movieid, MAX(relevance)
-                            FROM Genome_scores
-                            GROUP BY movieid
-                                HAVING tagid = %d
+                            SELECT gs.movieid, gs.tagid
+                            FROM Genome_Scores gs
+                            INNER JOIN (
+                                SELECT movieid, MAX(relevance) relevance
+                                FROM Genome_scores
+                                GROUP BY movieid
+                            ) gs_max_relevance
+                            ON gs.movieid = gs_max_relevance.movieid
+                                AND gs.relevance = gs_max_relevance.relevance
+                                AND gs.tagid = %d
                         )
                     )
              ORDER BY avg_rating DESC
          """ % (tagid)
 
+#Obter os filmes cujas tags por 
+# utilizador mais frequentes
+# coincidem com as de um
+# certo filme escolhido à priori
+## ** Procurar por filmes semelhantes ao selecionado **
+movieid = 1; 
+subsubquery = """SELECT m.movieid movieid, m.title title, 
+                            TRIM(LOWER(t.tag)) tag, 
+                            COUNT(TRIM(LOWER(t.tag))) counts
+                    FROM Movies m, Tags t
+                    WHERE m.movieid = t.movieid
+                    GROUP BY m.movieid, TRIM(LOWER(t.tag))"""
+subquery = """SELECT counts_table.movieid, counts_table.title, counts_table.tag
+                FROM (
+                    %s
+                    ) counts_table
+                INNER JOIN (
+                    SELECT movieid, title, tag, MAX(counts) counts
+                    FROM (
+                        %s
+                    ) 
+                    GROUP BY movieid 
+                ) counts_max
+                ON counts_table.movieid = counts_max.movieid
+                    AND counts_table.counts = counts_max.counts
+""" % (subsubquery, subsubquery)
+query2 = """
+            SELECT tbl.movieid, tbl.title, t.movieid, t.title
+            FROM (
+                %s
+            ) tbl
+            LEFT JOIN (
+                %s
+            ) t
+            ON tbl.tag = t.tag
+                AND tbl.movieid!=t.movieid
+            WHERE tbl.movieid=%d
+""" % (subquery, subquery, movieid)
+
 try:
     cur = db.cursor()
     start = time()
-    cur.execute(query)
+    cur.execute(query2)
     print(time() - start)
 
     input()
