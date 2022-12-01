21e32305a6aa1ccbfb12903d2fcc8c488400c516
Joel Oliveira <58565967+joelpoliveira@users.noreply.github.com>
2022-12-01 18:10:53 +0000

    add file to get ratings document for a movie, with out movieId field or _id field ( not wanted for the case )

---
+++ src/mongodb/update_movies_ratings.py
@@ -0,0 +1,36 @@
+import json
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Project_BDA
+
+coll_gen = db["Genome_Scores"]
+coll_ratings = db["Ratings"]
+coll_movies = db["Movies"]
+coll_tags = db["Tags"]
+
+query = [
+    {"$match" : {"_id" : 1}},
+    {"$lookup" : {
+        "from" : "Ratings",
+        "localField" : "ratings",
+        "foreignField" : "_id",
+        "as" : "ratings"
+    }},
+    {"$unwind" : "$ratings"},
+    {"$project" : {
+        "_id" : 0,
+        "ratings" : 1
+    }}
+]
+docs = coll_movies.aggregate(query)
+x = {}
+
+def poper(x : dict, *args):
+    for key in args:
+        x.pop(key)
+    return x
+
+ratings = list(map(lambda x: poper(x, "_id", "movieId"), map(lambda x: x["ratings"], docs)))
+with open("./data/movie_1_ratings.json","w") as f:
+    json.dump(ratings, f)
