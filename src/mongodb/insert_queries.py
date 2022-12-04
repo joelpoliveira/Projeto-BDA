15752705084af093ab0d6b12707c56bc7a26493c
Diogo Araújo <61624282+DiogoAraujoHUB@users.noreply.github.com>
2022-12-04 12:43:35 +0000

    :add - Added Mongo DB Insert and Update Queries
    Added MongoDB Insert and Update queries. Update had already been previously added however the insert queries were created based on the SQLlite insert queries i created.

--- src/mongodb/insert_queries.py
+++ src/mongodb/insert_queries.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 from time import time
-import json
 
 client = MongoClient()
 db = client.Project_BDA
@@ -10,17 +9,39 @@ coll_ratings = db["Ratings"]
 coll_movies = db["Movies"]
 coll_tags = db["Tags"]
 
-# userid = 134; rating = 3.5;
-# query = [
-#     { "userId" : userid },
-#     { "$set" : {"rating" : rating}}
-# ]
+movieid = 999999
+title = "\'Rise of the Fallen Lizards (2022)\'"
+genres = "\'Action|Horror|Lizard\'"
+imdb = 999999
+tmdb = 999999
+query1 = { "movieid": movieid,
+           "title": title,
+           "genres": genres,
+           "imdbid": imdb,
+           "tmdbid": tmdb }
 
-# start = time()
-# coll_ratings.update_many(*query)
-# print(time() - start)
+userid = 134
+rating = 3.5
+ts = 999999
+query2 = { "userid": userid,
+           "movieid": movieid,
+           "rating": rating,
+           "ts": ts }
 
-with open("./data/movie_1_ratings.json", "r") as f:
-    movie_1_ratings = json.load(f)
 
-coll_movies.update_one({"_id" : 1}, {"$set" : {"ratings" : movie_1_ratings}})
+tagid = 9999999
+tag = "\'Lizard\'"
+ts = 1000000
+query3 = { "userid": userid,
+           "movieid": movieid,
+           "tag": tag,
+           "ts": ts,
+           "tagid": tagid }
+
+
+#Inserts queries into different collections (uncomment others to add them aswell)
+start = time()
+coll_movies.insert_one(query1)
+#coll_ratings.insert_one(query2)
+#coll_tags.insert_one(query3)
+print(time() - start)
